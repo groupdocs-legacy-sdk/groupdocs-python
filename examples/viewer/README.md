f44c55097d573fe587727cc207db12d8cd6f25d3
averjr <averkiev.jr@gmail.com>
2012-10-02 17:17:46 +0300

    Viewer example added

---
+++ examples/viewer/README.md
@@ -0,0 +1,12 @@
+Installation instruction
+
+1) Create a Virtualenv http://pypi.python.org/pypi/virtualenv:
+virtualenv venv --distribute
+
+2) To activate local environment:
+source venv/bin/activate
+
+3) To install all requrments:
+pip install -r requirements.txt
+
+4) To start app: python app.py
--- examples/viewer/app.py
+++ examples/viewer/app.py
@@ -6,7 +6,6 @@ from pyramid.view import view_config
 
 from groupdocs.api.APIClient import APIClient
 from groupdocs.api.StorageAPI import StorageAPI
-from groupdocs.api.AntAPI import AntAPI
 import groupdocs.model
 	
 
@@ -14,9 +13,8 @@ def index(request):
     return {}
 
 def upload(request):
-	session = request.session
-	session['client_id'] = request.POST['client_id']
-	session['private_key'] = request.POST['private_key']
+	client_id = request.POST['client_id']
+	private_key = request.POST['private_key']
 	
 	input_file = request.POST['file']
 
@@ -37,41 +35,24 @@ def upload(request):
 		output_file.write(data)
 	output_file.close()	
 
-	apiClient = APIClient(session['private_key'], "https://api.groupdocs.com/v2.0")
-	response = StorageAPI(apiClient).Upload(session['client_id'], input_file.filename, "uploaded from python client library", 'file://' + str(file_path))
-	session['guid'] = response.result.guid
+	apiClient = APIClient(private_key, "https://api.groupdocs.com/v2.0")
+	response = StorageAPI(apiClient).Upload(client_id, input_file.filename, "uploaded from python client library", 'file://' + str(file_path))
 
 	os.remove(file_path)
 	return render_to_response('__main__:annotation.pt',
-                              {'guid':session['guid']},
+                              {'guid':response.result.guid},
                               request=request)
 
-def annotation(request):
-	session = request.session
-	apiClient = APIClient(session['private_key'], "https://api.groupdocs.com/v2.0")
-	response = AntAPI(apiClient).ListAnnotations(session['client_id'], session['guid'])
-	
-	output = ''
-	for annotation in response.result.annotations:
-		replies = []
-		for reply in annotation.replies:
-			replies.append(reply.userName + ": " + reply.text)
-		output += "Annotation Type: " +  str(annotation.type) + " -- Replies: " + str(replies) + "<br/>"
-	
-	return output
 
 if __name__ == '__main__':
     config = Configurator()
 
-    config.include("pyramid_beaker")
     config.add_route('index', '/')
     config.add_route('upload', '/upload')
-    config.add_route('annotation', '/annotation')
     
     config.add_view(index, route_name='index', renderer='__main__:upload_form.pt')
     config.add_view(upload, route_name='upload')
-    config.add_view(annotation, route_name='annotation', renderer='string')
-    
+        
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
