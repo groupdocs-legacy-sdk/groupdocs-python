121eabe86c7801dc4a90f825b37a79e0f02b4619
Aleksey Permyakov <aleksey.permyakov@groupdocs.com>
2012-11-21 09:32:38 +0200

    Finished Sample 7. Added Sample 8.

--- examples/api-samples/app.py
+++ examples/api-samples/app.py
@@ -1,9 +1,10 @@
 from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
 
 from pyramid.renderers import render_to_response
+from pyramid.config import Configurator
     
 import inc_samples.sample7 as sample7
+import inc_samples.sample8 as sample8
 
 def index(request):
     return {}
@@ -13,9 +14,11 @@ if __name__ == '__main__':
 
     config.add_route('index', '/')
     config.add_route('sample7', '/sample7')
+    config.add_route('sample8', '/sample8')
 
     config.add_view(index, route_name='index', renderer='__main__:templates/index.pt')
     config.add_view(sample7.sample7, route_name='sample7')
+    config.add_view(sample8.sample8, route_name='sample8')
         
     config.add_static_view(name='/', path='templates/')
     app = config.make_wsgi_app()
--- examples/api-samples/inc_samples/sample7.py
+++ examples/api-samples/inc_samples/sample7.py
@@ -1,4 +1,5 @@
 import base64
+
 from pyramid.renderers import render_to_response
 
 from groupdocs.ApiClient import ApiClient
@@ -6,6 +7,7 @@ from groupdocs.StorageApi import StorageApi
 from groupdocs.MgmtApi import MgmtApi
 from groupdocs.GroupDocsRequestSigner import GroupDocsRequestSigner
 
+# Sample 7
 def sample7(request):
     clientId = request.POST.get("client_id")
     privateKey = request.POST.get("private_key")
@@ -17,16 +19,21 @@ def sample7(request):
     apiClient = ApiClient(signer)
     mgmtApi = MgmtApi(apiClient)
     api = StorageApi(apiClient)
-    files = api.ListEntities(clientId, "", extended = True); # geting all Entities from curent user
+    try:
+        files = api.ListEntities(clientId, "", extended = True); # geting all Entities from curent user
+    except Exception, e:
+        return render_to_response('__main__:templates/sample7.pt', 
+                                  { 'error' : str(e) })
+
     thumbnail = '';
     name = '';
     for i in range(len(files.result.files)):
-        if files.result.files[i].thumbnail != "":
+        if files.result.files[i].thumbnail != str(None):
             fp = open('templates/thumbnail' + str(i) + '.jpg', 'wb')
             fp.write(base64.b64decode(files.result.files[i].thumbnail))
             fp.close()
             name = files.result.files[i].name
-            thumbnail += '<img src= "thumbnail' + str(i) + '.jpg", width="40px", height="40px">' + files.result.files[i].name + '</img> <br>'
+            thumbnail += '<img src= "thumbnail' + str(i) + '.jpg", width="40px", height="40px">' + files.result.files[i].name + '</img> <br />'
 
     return render_to_response('__main__:templates/sample7.pt', 
                               { 'thumbnailList' : thumbnail, 'userId' : clientId, 'privateKey' : privateKey }, 
