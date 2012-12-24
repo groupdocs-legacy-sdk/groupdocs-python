7116bae4de5cc5bf763175d90b824a1782e6663f
Pavel-Teplitsky <pavel.teplitsky@groupdocs.com>
2012-12-24 17:00:30 +0200

    updated comments and added generated with docco documentation

--- examples/api-samples/inc_samples/sample3.py
+++ examples/api-samples/inc_samples/sample3.py
@@ -1,3 +1,8 @@
+#<i>This sample will show how to use <b>Upload</b> method from Storage Api to upload file to GroupDocs Storage </i>
+
+####Set variables and get POST data
+
+#Import of classes from libraries
 import base64
 import os
 from pyramid.renderers import render_to_response
@@ -11,41 +16,45 @@ from groupdocs.GroupDocsRequestSigner import GroupDocsRequestSigner
 def IsNotNull(value):
     return value is not None and len(value) > 0
 
-# Sample 3
+####Set variables and get POST data
 def sample3(request):
     clientId = request.POST.get('client_id')
     privateKey = request.POST.get('private_key')
     inputFile = request.POST.get('file')
-
+    # Checking clientId and privateKey
     if IsNotNull(clientId) == False or IsNotNull(privateKey) == False:
         return render_to_response('__main__:templates/sample3.pt', 
                                   { 'error' : 'You do not enter your User Id or Private Key' })
+####Create Signer, ApiClient and Storage Api objects
 
+#Create signer object
     signer = GroupDocsRequestSigner(privateKey)
+#Create apiClient object
     apiClient = ApiClient(signer)
+#Create Storage Api object
     api = StorageApi(apiClient)
-#    apiClient.setDebug(True)
 
     try:
-        # a hack to get uploaded file size
+        #A hack to get uploaded file size
         inputFile.file.seek(0, 2)
         fileSize = inputFile.file.tell()
         inputFile.file.seek(0)
         
         fs = FileStream.fromStream(inputFile.file, fileSize)
-        #~ import pdb;  pdb.set_trace()
+        ####Make a request to Storage API using clientId
 
+        #Upload file to current user storage
         response = api.Upload(clientId, inputFile.filename, fs)
-
+        #Generation of Embeded Viewer URL with uploaded file GuId
         iframe = '<iframe src="https://apps.groupdocs.com/document-viewer/embed/' + response.result.guid + '" frameborder="0" width="720" height="600""></iframe>'
-        massage = '<p>File was uploaded to GroupDocs. Here you can see your <strong>' + inputFile.filename + '</strong> file in the GroupDocs Embedded Viewer.</p>'
+        message = '<p>File was uploaded to GroupDocs. Here you can see your <strong>' + inputFile.filename + '</strong> file in the GroupDocs Embedded Viewer.</p>'
     except Exception, e:
         return render_to_response('__main__:templates/sample3.pt', 
                                   { 'error' : str(e) })
-
+    #If request was successfull - set variables for template
     return render_to_response('__main__:templates/sample3.pt', 
                               { 'userId' : clientId, 
                                'privateKey' : privateKey, 
                                'iframe' : iframe,
-                               'massage' : massage}, 
+                               'message' : message},
                               request=request)
--- examples/api-samples/inc_samples/sample4.py
+++ examples/api-samples/inc_samples/sample4.py
@@ -1,3 +1,6 @@
+#<i>This sample will show how to use <b>GetFile</b> method from Storage Api to download a file from GroupDocs Storage</i>
+
+#Import of classes from libraries
 import base64
 import os
 import shutil
@@ -12,53 +15,65 @@ from groupdocs.GroupDocsRequestSigner import GroupDocsRequestSigner
 def IsNotNull(value):
     return value is not None and len(value) > 0
 
-# Sample 4
+####Set variables and get POST data
 def sample4(request):
     clientId = request.POST.get('client_id')
     privateKey = request.POST.get('private_key')
     
     file_id = request.POST.get('file_id')
-
+    # Checking clientId, privateKey and file_Id
     if IsNotNull(clientId) == False or IsNotNull(privateKey) == False or IsNotNull(file_id) == False:
         return render_to_response('__main__:templates/sample4.pt', 
                                   { 'error' : 'You do not enter all parameters' })
 
+    ####Create Signer, ApiClient and Storage Api objects
+
+    #Create signer object
     signer = GroupDocsRequestSigner(privateKey)
+    #Create apiClient object
     apiClient = ApiClient(signer)
+    #Create Storage Api object
     api = StorageApi(apiClient)
-               
+
+    ####Make a request to Storage API using clientId
+
     try:
-        
+        #Obtaining all Entities from current user
         files = api.ListEntities(userId = clientId, path = '', pageIndex = 0)
+        #Selecting file names
         for item in files.result.files: #selecting file names
+            #Obtaining file name for entered file Id
            if item.guid == file_id:
                fileName = item.name
-               
+
+        ####Make a request to Storage Api for dowloading file
+
+        #Obtaining file stream of downloading file and definition of folder where to download file
         currentDir = os.path.dirname(os.path.realpath(__file__))
         
         newpath = currentDir + "/../tmp/"
         if not os.path.exists(newpath):
-            os.makedirs(newpath)   
-         
+            os.makedirs(newpath)
+        #Downlaoding of file
         fs = api.GetFile(clientId, file_id);
 
         if fs:
-            #~ import pdb;  pdb.set_trace()
+
             filePath = newpath + fileName
         
             with open(filePath, 'wb') as fp:
                 shutil.copyfileobj(fs.inputStream, fp)
             
-            massage = '<font color="green">File was downloaded to the <font color="blue">' + filePath + '</font> folder</font> <br />';
+            message = '<font color="green">File was downloaded to the <font color="blue">' + filePath + '</font> folder</font> <br />';
         else:
 			raise Exception('Wrong file ID!')
     except Exception, e:
         return render_to_response('__main__:templates/sample4.pt', 
                                   { 'error' : str(e) })
-
+    #If request was successfull - set message variable for template
     return render_to_response('__main__:templates/sample4.pt', 
                               { 'userId' : clientId, 
                                'privateKey' : privateKey, 
                                'file_Id' : file_id, 
-                               'massage' : massage }, 
+                               'message' : message },
                               request=request)
